e0398af1b4f4254471f5cbe3e81c29eeeb720f3c
DaveJ <dave@davejeffery.com>
2015-03-31 20:01:08 +0100

    Fix module name in build script

--- gulp/build.js
+++ gulp/build.js
@@ -18,7 +18,7 @@ module.exports = function(options) {
         quotes: true
       }))
       .pipe($.angularTemplatecache('templateCacheHtml.js', {
-        module: 'site'
+        module: 'corkdev'
       }))
       .pipe(gulp.dest(options.tmp + '/partials/'));
   });
